903ca6c4b3129a952d33f170e480cf42b86b8894
hkm0629 <hkm0629@uangel.com>
2022-06-16 00:04:51 +0900

    Spring DB Access Technique

--- 01_hello-spring/src/main/java/hello/hellospring/SpringConfig.java
+++ 01_hello-spring/src/main/java/hello/hellospring/SpringConfig.java
@@ -5,34 +5,29 @@ import hello.hellospring.service.MemberService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-
-import javax.persistence.Entity;
-import javax.persistence.EntityManager;
-import javax.sql.DataSource;
-
 /**
  * @author kangmoo Heo
  */
 @Configuration
 public class SpringConfig {
 
-    private EntityManager em;
+    public final MemberRepository memberRepository;
 
     @Autowired
-    public SpringConfig(EntityManager em) {
-        this.em = em;
+    public SpringConfig(MemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
     }
 
     @Bean
     public MemberService memberService(){
-        return new MemberService(memberRepository());
+        return new MemberService(memberRepository);
     }
 
-    @Bean
-    public MemberRepository memberRepository(){
+//    @Bean
+//    public MemberRepository memberRepository(){
 //        return new MemoryMemberRepository();
 //        return new JdbcMemberRepository(dataSource);
 //        return new JdbcTemplateMemberRepository(dataSource);
-        return new JpaMemberRepository(em);
-    }
+//        return new JpaMemberRepository(em);
+//    }
 }

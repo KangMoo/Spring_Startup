71c955e73e3f163a56ea63208179f13615027c74
hkm0629 <hkm0629@uangel.com>
2022-06-29 21:38:51 +0900

    AutoInjection Test

--- 02_Spring Core Basic/core/src/main/java/hello/core/order/OrderServiceImpl.java
+++ 02_Spring Core Basic/core/src/main/java/hello/core/order/OrderServiceImpl.java
@@ -12,11 +12,12 @@ import org.springframework.stereotype.Component;
 @Component
 public class OrderServiceImpl implements OrderService {
 
-    private final MemberRepository memberRepository;
-    private final DiscountPolicy discountPolicy;
+    private MemberRepository memberRepository;
+    private DiscountPolicy discountPolicy;
 
     @Autowired
     public OrderServiceImpl(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
+        System.out.println("Dependency auto injected by Constructor");
         this.memberRepository = memberRepository;
         this.discountPolicy = discountPolicy;
     }
@@ -26,5 +27,19 @@ public class OrderServiceImpl implements OrderService {
         Member member = memberRepository.findById(memberId);
         int discountPrice = discountPolicy.discount(member, itemPrice);
         return new Order(memberId, itemName, itemPrice, discountPrice);
+    }
+
+    @Autowired
+    public OrderServiceImpl setMemberRepository(MemberRepository memberRepository) {
+        System.out.println("MemberRepository Dependency auto injected by Setter");
+        this.memberRepository = memberRepository;
+        return this;
+    }
+
+    @Autowired
+    public OrderServiceImpl setDiscountPolicy(DiscountPolicy discountPolicy) {
+        System.out.println("DiscountPolicy Dependency auto injected by Setter");
+        this.discountPolicy = discountPolicy;
+        return this;
     }
 }
--- 02_Spring Core Basic/core/src/test/java/hello/core/scan/AutoAppConfigTest.java
+++ 02_Spring Core Basic/core/src/test/java/hello/core/scan/AutoAppConfigTest.java
@@ -2,9 +2,7 @@ package hello.core.scan;
 
 import hello.core.AutoAppConfig;
 import hello.core.member.MemberService;
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.autoconfigure.filter.TypeExcludeFilters;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 import static org.assertj.core.api.Assertions.*;
